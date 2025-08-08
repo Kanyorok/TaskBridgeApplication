3d1d45f734266333d9404451e4c4c7e998a16a10
Kanyorok <rmjesusaddict@gmail.com>
2025-08-08 12:14:54 +0300

    Hotfix the code with the correct configurations

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,4 +1,6 @@
-server "172.16.2.15", user: "taskbridgetest", roles: %w{app db web}
+# config/deploy/staging.rb
 
+server "172.16.2.15", user: "taskbridgetest", roles: %w{app db web}
+set :stage, :staging
 set :rails_env, "staging"
-set :branch, "dev" 
+set :branch, "dev"
